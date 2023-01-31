03ef22db30fff635008510ceece92c98b8ce721d
sharifgk <sharif_khan@live.co.uk>
2023-01-31 11:48:53 +0000

    2nd test transDB passed

--- spec/transactionDB.spec.js
+++ spec/transactionDB.spec.js
@@ -19,6 +19,17 @@ describe('transactionDB Tests', () => {
 
         });
 
+        it('Transaction details match the arguments passed', () => {
+            testTranDB.addTransaction("01/01/2000", "deposit", 1000, 1000);
+
+            expect(testTranDB.getTransactions()[0]).toEqual({
+                date: "01/01/2000",
+                type: "deposit",
+                amount: 1000,
+                balance: 1000,
+            })
+        })
+
 
 
     });
