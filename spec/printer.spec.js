b406a9c6d945a681bad2dc4d188cfc8add60b6f9
sharifgk <sharif_khan@live.co.uk>
2023-02-01 11:30:34 +0000

    printer tests partially passed

--- spec/printer.spec.js
+++ spec/printer.spec.js
@@ -1,32 +1,40 @@
 import Printer from "../src/Printer.js"
 
-xdescribe('Printer test', () => {
+describe('Printer test', () => {
     let testPrinter;
+    let consoleSpy;
 
     beforeEach(() => {
         testPrinter = new Printer();
+        consoleSpy = spyOn(console, `log`);
     });
 
-    describe('Printer functionality test', () => {
-        it('Formats and outputs the transaction data', () => {
-
-            spyOn(console, `log`);
-
-            const transactions =
-                [{ date: `01/01/2000`, credit: 1000, debit: ``, balance: 1000 },
-                { date: `02/01/2000`, credit: ``, debit: 500, balance: 500 }];
-            testPrinter.print(transactions);
-
-            expect(console.log).toHaveBeenCalledWith(`date       || credit  || debit  || balance`);
-            expect(console.log).toHaveBeenCalledWith(`01/01/2000 || 1000.00 ||        || 1000.00`);
-            expect(console.log).toHaveBeenCalledWith(`02/01/2000 ||         || 500.00 ||  500.00`);
 
 
+    it('Top row is printed correctly', () => {
 
-        })
-    })
+        testPrinter.print([]);
+        expect(consoleSpy).toHaveBeenCalledWith(`date       || credit  || debit || balance`);
+
+    });
+
+
+    // it('should print transactions in the expected format', () => {
+
+    //     testPrinter.print([
+    //         { date: `01/01/2000`, type: `deposit`, amount: 1000, balance: 1000 },
+    //         { date: `02/01/2000`, type: `withdraw`, amount: 500, balance: 500 }
+    //     ]);
+
+    //     expect(consoleSpy).toHaveBeenCalledWith(`01/01/2000 || 1000.00 ||       || 1000.00`);
+    //     expect(consoleSpy).toHaveBeenCalledWith(`02/01/2000 ||         || 500.00||  500.00`);
+
+    // })
 
 
 
 
-})
+
+
+
+});
